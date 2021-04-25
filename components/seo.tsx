4e28f8979e4ab402adc5de7a4083d89bf676fd01
JcdeA <jcde@jcde.xyz>
2021-04-25 21:03:16 +0900

    preload images

--- components/seo.tsx
+++ components/seo.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 const SEO: React.FC = () => {
   return (
     <Head>
+    	<link rel="prefetch" href="/static/images/light.svg">
+	<link rel="prefetch" href="/static/images/mountain.svg">
       <title>Team int</title>
       <meta name="description" content="Team Int 공식 웹사이트 - We live to code"></meta>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
