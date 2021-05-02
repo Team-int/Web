6c7c20423beac872b6678969353880a859517f8c
JcdeA <jcde@jcde.xyz>
2021-05-02 13:41:03 +0900

    improve preloading

--- components/seo.tsx
+++ components/seo.tsx
@@ -2,8 +2,8 @@ import Head from 'next/head'
 const SEO: React.FC = () => {
   return (
     <Head>
-      <link rel="preload" href="/static/images/light.svg" />
-      <link rel="preload" href="/static/images/mountain.svg" />
+      <link rel="preload" as="image" href="/static/images/light.svg" />
+      <link rel="preload" as="image" href="/static/images/mountain.svg" />
       <title>Team int</title>
       <meta name="description" content="Team Int 공식 웹사이트 - We live to code"></meta>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
