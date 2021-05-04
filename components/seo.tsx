4e472ab7f3bb04dd1ca159858eea015c7333e026
JcdeA <jcde@jcde.xyz>
2021-05-04 20:04:21 +0900

    add preview

--- components/seo.tsx
+++ components/seo.tsx
@@ -5,6 +5,7 @@ const SEO: React.FC = () => {
       <link rel="preload" as="image" href="/static/images/light.svg" />
       <link rel="preload" as="image" href="/static/images/mountain.svg" />
       <title>Team int</title>
+      <meta property="og:image" content="/static/images/preview.webp" />
       <meta name="description" content="Team Int 공식 웹사이트 - We live to code"></meta>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
     </Head>
